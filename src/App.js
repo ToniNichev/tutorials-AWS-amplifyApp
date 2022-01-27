f872a9786ca3de37118bb088570dd7bd04eed090
ToniNichev <toni.nichev@gmail.com>
2022-01-27 10:35:16 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Hello from V2</h1>
+            <button onClick={signOut}>Sign out</button>
+          </header>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
